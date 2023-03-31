fb85655c1d79fa761493eb6dff09f23600acc3ed
Orianamartina <orianapettinelli04@gmail.com>
2023-03-30 23:05:00 -0300

    attempt 2

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import store from './redux/store';
 import {BrowserRouter} from "react-router-dom"
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+axios.defaults.baseURL = "proyecto-individual-henry-production-b263.up.railway.app" || "http://localhost:3001"
 
 ReactDOM.render(
   <Provider store = {store}>
